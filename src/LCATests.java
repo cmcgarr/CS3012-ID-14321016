e64c68f73067bb0e47704519556a3ef9cb0a4dc2
Ciaran McGarry <cmcgarr@tcd.ie>
2017-10-13 21:20:13 +0100

    Created new tests and have binaryTree.put() function working based on test results. This is suspicious given how inattentively I created that function, so the tests deserve a look in the honest light of morning

--- src/LCATests.java
+++ src/LCATests.java
@@ -10,15 +10,33 @@ public class LCATests {
 		new binaryTree<Integer, Integer>();
 	}
 
+	@Test
+	public void testIsEmpty(){
+
+		binaryTree<Integer, Integer> testBT = new binaryTree<Integer, Integer>();
+		assertEquals("Testing isEmpty on an empty tree", true, testBT.isEmpty());
+		testBT.put(null, 8, 8);
+		assertEquals("Testing isEmpty on a non-empty tree", false, testBT.isEmpty());
+	}
 
 	@Test
-	public void testPutGet(){
+	public void testPut() {
+		binaryTree<Integer, Integer> testBT = new binaryTree<Integer, Integer>();
+
+		assertEquals("Testing put function into empty binary tree", true, testBT.put(null, 7,7));
+		assertEquals("Testing put function into non-empty tree", true, testBT.put(7,8,8));
+		assertEquals("Testing put function for replacing old value", true, testBT.put(7,8,9));
+		assertEquals("Testing put function into non-empty tree for non-existent parent", false, testBT.put(10,8,8));
+	}
+
+	@Test
+	public void testGet(){
 		binaryTree<Integer, Integer> testBT = new binaryTree<Integer, Integer>();
 		int testValue = 7;
 
 		assertEquals("Test get on empty binary tree", null, testBT.get(testValue));
 
-		testBT.put(testValue,testValue);
+		testBT.put(null,testValue,testValue);
 
 		assertEquals("Test get on binary tree with only a root", (Integer)testValue, testBT.get(testValue));
 
@@ -34,7 +52,7 @@ public class LCATests {
 		binaryTree<Integer, Integer> testBT = new binaryTree<Integer, Integer>();
 
 
-		testBT.put(1,1);
+		testBT.put(null,1,1);
 		testBT.put(1,5,5);
 		testBT.put(5,3,3);
 		testBT.put(3,14,14);
--- src/binaryTree.java
+++ src/binaryTree.java
@@ -5,25 +5,123 @@ Thus this is not a Binary Search Tree, and new nodes are inserted based on who t
 In the event that two equal Keys are inserted under equal parents, the first key found is returned
 
 */
-//TODO: add documentation
+//TODO:
 
 public class binaryTree<Key extends Comparable<Key>, Value> {
 
+    private Node root;
+
+    private class Node{
+      private Key key;              //sorted by key
+      private Value value;          //associated data
+      private Node left, right, parent;     //leftChild and rightChild
+
+      public Node(Node parent, Key key, Value value){
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+      }
+    }
+
+    public boolean isEmpty(){
+      return (root == null);
+    }
+
     public Value get(Key key){
-    	
     	return null;
     }
 
-    public void put(Key key, Value value){
+    /**
+     *  Insert key-value pair into BST.
+     *  If key already exists under same parent, update with new value.
+     *
+     *  @param parent the key under which to insert the new key (set to null if new key is to be set as root)
+     *  @param key the key to insert
+     *  @param val the value associated with key
+     *
+     *TODO: tidy up and make functions
+     */
 
+    public boolean put(Key parent, Key key, Value value){
+
+      boolean success = false;
+      //if{
+        // if (value == null){
+          //success = delete(key);                TODO implement delete function
+        // }
+
+      //set as root ----TODO change to else if once delete is implemented----
+      if(parent == null){
+        //if root is empty, create new root
+        if (root == null){ root = new Node(null, key, value); }
+        //else update key and value of old root
+        else{
+          root.key = key;
+          root.value = value;
+        }
+        success = true;
+        return success;
+      }
+
+      Node target = search(root, parent);
+      Node newNode = new Node(target, key, value);
+
+      //null meaning not found
+      if(target == null){
+        success = false;
+      }
+      else{
+      //if the left node is empty, set left to be newNode
+          if(target.left == null){
+            target.left = newNode;
+            success = true;
+          }
+          //if left node is not empty, but shares newNode's key, update the value of the left node
+          else if(target.left.key.compareTo(newNode.key) == 0){
+            target.left.value = value;
+            success = true;
+          }
+          //if the right node is empty
+          else if(target.right == null){
+            target.right = newNode;
+            success = true;
+          }
+          //if the right node is not empty, but shares newNode's key, update the value of right node
+          else if(target.right.key.compareTo(newNode.key) == 0){
+            target.right.value = value;
+            success = true;
+          }
+          //in the case of there being no empty child or child of equal key
+          else{ success = false;}
+        }
+      return success;
     }
 
-    public void put(Key parent, Key key, Value value){
+    private Node search(Node x, Key key){
+      if(x == null){ return null; }
 
+      else{
+        //check if key has been found, returning node is so
+        if(x.key.compareTo(key) == 0){
+          return x;
+        }
+        else{
+          //search left child for key, returning result if found
+          Node leftSearch = search(x.left, key);
+          if(leftSearch != null){ return leftSearch; }
+
+          //search right child for key, returning result if found
+          Node rightSearch = search(x.right, key);
+          if(rightSearch != null){ return rightSearch; }
+        }
+      }
+
+      return null;
     }
+
 
     public Value LCA(Key key1, Key key2){
-      
+
     	return null;
     }
 
